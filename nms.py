e670314e84faec0e75a5d2f43efbce06f1fe292b
ha4219 <ha4219@naver.com>
2022-08-16 20:50:10 +0900

    log

--- nms.py
+++ nms.py
@@ -48,6 +48,7 @@ def non_max_suppression(prediction, conf_thres=0.25, iou_thres=0.45, classes=Non
     nc = prediction.shape[2] - 5  # number of classes
     xc = prediction[..., 4] > conf_thres  # candidates
     print(xc.sum())
+    print(prediction[..., 4])
 
     # Checks
     assert 0 <= conf_thres <= 1, f'Invalid Confidence threshold {conf_thres}, valid values are between 0.0 and 1.0'
